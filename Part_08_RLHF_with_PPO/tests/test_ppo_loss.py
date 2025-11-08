ff531b154d3346838fdfcdc566844bc659af5105
ShwaTech <shwakesh13579@gmail.com>
2025-11-08 20:39:22 +0200

    Part 8 - Completed Successfully!

--- Part_08_RLHF_with_PPO/tests/test_ppo_loss.py
+++ Part_08_RLHF_with_PPO/tests/test_ppo_loss.py
@@ -8,12 +8,12 @@ def test_clipped_objective_behaves():
     old_logp = torch.zeros(N)
     new_logp = torch.log(torch.full((N,), 1.2))  # ratio=1.2
     
-    adv      = torch.ones(N)
+    adv = torch.ones(N)
     
     new_v = torch.zeros(N)
     old_v = torch.zeros(N)
     
-    ret  = torch.ones(N)
+    ret = torch.ones(N)
     
     out = ppo_losses(new_logp, old_logp, adv, new_v, old_v, ret, clip_ratio=0.1)
     
